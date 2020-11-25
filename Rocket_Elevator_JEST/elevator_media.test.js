441cbfd0881ebaa7625d0464e66da49bc0a83949
Will Jacques <willy321@hotmail.com>
2020-11-25 16:11:21 -0500

    starting_getContent2

--- Rocket_Elevator_JEST/elevator_media.test.js
+++ Rocket_Elevator_JEST/elevator_media.test.js
@@ -1,8 +1,13 @@
 const Streamer = require ('./modules/ElevatorMedia');
 
-test('return color', () => {
-    let mycar = new Streamer( 'Dodge', 'Grand Caravan', 2012, 'Grey' );
-    expect(mycar.color).toBe('Grey');
-    mycar.getContent('Pink');
-    expect(mycar.color).toBe('Pink');
+// test('return color', () => {
+//     let mycar = new Streamer( 'Dodge', 'Grand Caravan', 2012, 'Grey' );
+//     expect(mycar.color).toBe('Grey');
+//     mycar.getContent('Pink');
+//     expect(mycar.color).toBe('Pink');
+// })
+
+test('return best president', () => {
+    let myfacts = new Streamer()
+    expect(myfacts.getcontent2()).toBe('Chuck Norris for president !');
 })
--- Rocket_Elevator_JEST/modules/ElevatorMedia.js
+++ Rocket_Elevator_JEST/modules/ElevatorMedia.js
@@ -1,12 +1,16 @@
 class Streamer {
-    constructor( make, model, year, color ) {
-      this.make = make;
-      this.model = model;
-      this.year = year;
-      this.color = color;
-    }
-    getContent( color ) {
-      this.color = color;
+    // constructor( make, model, year, color ) {
+    //   this.make = make;
+    //   this.model = model;
+    //   this.year = year;
+    //   this.color = color;
+    // }
+    // getContent( color ) {
+    //   this.color = color;
+    // }
+    getcontent2() {
+        var fact = "Chuck Norris for president !"
+        return fact
     }
 }
 
