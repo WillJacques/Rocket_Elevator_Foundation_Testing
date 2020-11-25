6613e82c1b7277f3c8fcfef4986c96f76edb93a4
Will Jacques <willy321@hotmail.com>
2020-11-25 16:24:04 -0500

    push_before_ajax

--- Rocket_Elevator_JEST/elevator_media.test.js
+++ Rocket_Elevator_JEST/elevator_media.test.js
@@ -1,11 +1,11 @@
 const Streamer = require ('./modules/ElevatorMedia');
 
-// test('return color', () => {
-//     let mycar = new Streamer( 'Dodge', 'Grand Caravan', 2012, 'Grey' );
-//     expect(mycar.color).toBe('Grey');
-//     mycar.getContent('Pink');
-//     expect(mycar.color).toBe('Pink');
-// })
+test('return color', () => {
+    let mycar = new Streamer( 'Dodge', 'Grand Caravan', 2012, 'Grey' );
+    expect(mycar.color).toBe('Grey');
+    mycar.getContent('Pink');
+    expect(mycar.color).toBe('Pink');
+})
 
 test('return best president', () => {
     let myfacts = new Streamer()
--- Rocket_Elevator_JEST/modules/ElevatorMedia.js
+++ Rocket_Elevator_JEST/modules/ElevatorMedia.js
@@ -1,13 +1,13 @@
 class Streamer {
-    // constructor( make, model, year, color ) {
-    //   this.make = make;
-    //   this.model = model;
-    //   this.year = year;
-    //   this.color = color;
-    // }
-    // getContent( color ) {
-    //   this.color = color;
-    // }
+    constructor( make, model, year, color ) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+      this.color = color;
+    }
+    getContent( color ) {
+      this.color = color;
+    }
     getcontent2() {
         var fact = "Chuck Norris for president !"
         return fact
@@ -15,3 +15,23 @@ class Streamer {
 }
 
 module.exports = Streamer;
+
+// $.ajax({
+//     url: "/building",
+//     type: "GET",  
+//     dataType: "json",
+//     data: {customer: customer},
+//     error: function (xhr, status, error) {
+//         console.error('AJAX Error: ' + status + error);
+//     },
+//     success: function (response) {
+//         console.log(response);
+//         var buildings = response["buildings"];
+//         $("#intervention_building_id").empty();
+
+//         $("#intervention_building_id").append('<option value="">Select Building</option>');
+//         for(var i = 0; i < buildings.length; i++){
+//             $("#intervention_building_id").append('<option value="' + buildings[i]["id"] + '">' +buildings[i]["address_of_the_building"] + '</option>');
+//         }
+//     }
+// });
