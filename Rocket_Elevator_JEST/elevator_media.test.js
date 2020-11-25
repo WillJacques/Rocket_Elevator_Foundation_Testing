c6d748556566dcb5d92ff272b4df135c2620d065
Will Jacques <willy321@hotmail.com>
2020-11-25 12:58:22 -0500

    mycar

--- Rocket_Elevator_JEST/elevator_media.test.js
+++ Rocket_Elevator_JEST/elevator_media.test.js
@@ -1,8 +1,8 @@
 const Streamer = require ('./modules/ElevatorMedia');
 
 test('return color', () => {
-    let subaru = new Streamer( 'Subaru', 'Outback', 2005, 'Grey' );
-    expect(subaru.color).toBe('Grey');
-    subaru.getContent('Red');
-    expect(subaru.color).toBe('Red');
+    let mycar = new Streamer( 'Dodge', 'Grand Caravan', 2012, 'Grey' );
+    expect(mycar.color).toBe('Grey');
+    mycar.getContent('Pink');
+    expect(mycar.color).toBe('Pink');
 })
